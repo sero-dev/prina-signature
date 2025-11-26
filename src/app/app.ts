bc89f9486fa331a6580e646cb808cd75f0266f5b
Sean Rodriguez <sean.rodriguez@outlook.com>
2025-11-26 01:40:25 -0500

    Add DM Serif Text font as default font family; Update landing page with content

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
+    <main class="min-h-screen flex flex-col items-center justify-center p-4">
+      <section>
+        <h1 class="md:text-9xl text-7xl">
+          <div>Prina</div>
+          <div>Signature</div>
+        </h1>
+      </section>
+      <small class="mt-10 text-3xl">Clothing, Mirrors, Beauty</small>
 
-    <router-outlet />
+      <!-- <section class="mt-10 flex flex-col gap-4">
+        <button>Instagram</button>
+        <button>Email</button>
+        <button>Phone</button>
+        <button>Address</button>
+      </section> -->
+    </main>
   `,
   styles: [],
 })
